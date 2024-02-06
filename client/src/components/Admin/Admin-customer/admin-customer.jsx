1ae4a4828d961d9b5cbf5654d61d9721fd4a8f10
Anilvarmathirumalaraju <anilvarmathirumalaraju@gmail.com>
2024-02-06 13:53:22 +0530

    changes done on  customers page

--- client/src/components/Admin/Admin-customer/admin-customer.jsx
+++ client/src/components/Admin/Admin-customer/admin-customer.jsx
@@ -1,199 +1,56 @@
-import './admin-customer.css'
-import profileimg from '../images/profile.png'
-import dots from '../images/dots.png'
-import Adminheader from '../adminheader'
+import React, { useState, useEffect } from 'react';
+import './admin-customer.css';
+import profileimg from '../images/profile.png';
+import dots from '../images/dots.png';
+import Adminheader from '../adminheader';
 
-const Admincustomer = (()=>{
-    return(
-        <>
-        
-           <Adminheader/>
-            <div className="admincustomer">
-                
-                <div className="New-Customer">
-                   <h1>New Customers</h1>
-                   <div className='admincustomer-details'>
-                        
-                        <div className='customer-sub-details'>
-                             <img src={profileimg} alt='profileimg' className='profile-img'/>
-                             <div className='customer-content'>
-                                 <p>Janaki Ramakrishna</p>
-                                 <p className='date'>Joined : 08/02/2024 </p>
-                             </div>
-                             
-                             <img src={dots} alt='dots' className='dotsimg'/>
-                        </div>
-                        <div className='customer-sub-details'>
-                             <img src={profileimg} alt='profileimg' className='profile-img'/>
-                             <div className='customer-content'>
-                                 <p>Janaki Ramakrishna</p>
-                                 <p className='date'>Joined : 08/02/2024 </p>
-                             </div>
-                             
-                             <img src={dots} alt='dots' className='dotsimg'/>
-                        </div>
-                        <div className='customer-sub-details'>
-                             <img src={profileimg} alt='profileimg' className='profile-img'/>
-                             <div className='customer-content'>
-                                 <p>Janaki Ramakrishna</p>
-                                 <p className='date'>Joined : 08/02/2024 </p>
-                             </div>
-                             
-                             <img src={dots} alt='dots' className='dotsimg'/>
-                        </div>
-                        <div className='customer-sub-details'>
-                             <img src={profileimg} alt='profileimg' className='profile-img'/>
-                             <div className='customer-content'>
-                                 <p>Janaki Ramakrishna</p>
-                                 <p className='date'>Joined : 08/02/2024 </p>
-                             </div>
-                             
-                             <img src={dots} alt='dots' className='dotsimg'/>
-                        </div>
-                        <div className='customer-sub-details'>
-                             <img src={profileimg} alt='profileimg' className='profile-img'/>
-                             <div className='customer-content'>
-                                 <p>Janaki Ramakrishna</p>
-                                 <p className='date'>Joined : 08/02/2024 </p>
-                             </div>
-                             
-                             <img src={dots} alt='dots' className='dotsimg'/>
-                        </div>
-                        <div className='customer-sub-details'>
-                             <img src={profileimg} alt='profileimg' className='profile-img'/>
-                             <div className='customer-content'>
-                                 <p>Janaki Ramakrishna</p>
-                                 <p className='date'>Joined : 08/02/2024 </p>
-                             </div>
-                             
-                             <img src={dots} alt='dots' className='dotsimg'/>
-                        </div>
-                   </div>
-                   
-                </div>
-                <div className="Repeat-Customer">
-                    <h1>Repeat Customers</h1>
-                    <div className='admincustomer-details'>
-                        
-                        <div className='customer-sub-details'>
-                             <img src={profileimg} alt='profileimg' className='profile-img'/>
-                             <div className='customer-content'>
-                                 <p>Janaki Ramakrishna</p>
-                                 <p className='date'>Joined : 08/02/2024 </p>
-                             </div>
-                             
-                             <img src={dots} alt='dots' className='dotsimg'/>
-                        </div>
-                        <div className='customer-sub-details'>
-                             <img src={profileimg} alt='profileimg' className='profile-img'/>
-                             <div className='customer-content'>
-                                 <p>Janaki Ramakrishna</p>
-                                 <p className='date'>Joined : 08/02/2024 </p>
-                             </div>
-                             
-                             <img src={dots} alt='dots' className='dotsimg'/>
-                        </div>
-                        <div className='customer-sub-details'>
-                             <img src={profileimg} alt='profileimg' className='profile-img'/>
-                             <div className='customer-content'>
-                                 <p>Janaki Ramakrishna</p>
-                                 <p className='date'>Joined : 08/02/2024 </p>
-                             </div>
-                             
-                             <img src={dots} alt='dots' className='dotsimg'/>
-                        </div>
-                        <div className='customer-sub-details'>
-                             <img src={profileimg} alt='profileimg' className='profile-img'/>
-                             <div className='customer-content'>
-                                 <p>Janaki Ramakrishna</p>
-                                 <p className='date'>Joined : 08/02/2024 </p>
-                             </div>
-                             
-                             <img src={dots} alt='dots' className='dotsimg'/>
-                        </div>
-                        <div className='customer-sub-details'>
-                             <img src={profileimg} alt='profileimg' className='profile-img'/>
-                             <div className='customer-content'>
-                                 <p>Janaki Ramakrishna</p>
-                                 <p className='date'>Joined : 08/02/2024 </p>
-                             </div>
-                             
-                             <img src={dots} alt='dots' className='dotsimg'/>
-                        </div>
-                        <div className='customer-sub-details'>
-                             <img src={profileimg} alt='profileimg' className='profile-img'/>
-                             <div className='customer-content'>
-                                 <p>Janaki Ramakrishna</p>
-                                 <p className='date'>Joined : 08/02/2024 </p>
-                             </div>
-                             
-                             <img src={dots} alt='dots' className='dotsimg'/>
-                        </div>
-                   </div>
+const Admincustomer = () => {
+  const [users, setUsers] = useState([]);
 
-                </div>
-                <div className="Old-Customer">
-                   <h1>Old Customers</h1>
-                   <div className='admincustomer-details'>
-                        
-                        <div className='customer-sub-details'>
-                             <img src={profileimg} alt='profileimg' className='profile-img'/>
-                             <div className='customer-content'>
-                                 <p>Janaki Ramakrishna</p>
-                                 <p className='date'>Joined : 08/02/2024 </p>
-                             </div>
-                             
-                             <img src={dots} alt='dots' className='dotsimg'/>
-                        </div>
-                        <div className='customer-sub-details'>
-                             <img src={profileimg} alt='profileimg' className='profile-img'/>
-                             <div className='customer-content'>
-                                 <p>Janaki Ramakrishna</p>
-                                 <p className='date'>Joined : 08/02/2024 </p>
-                             </div>
-                             
-                             <img src={dots} alt='dots' className='dotsimg'/>
-                        </div>
-                        <div className='customer-sub-details'>
-                             <img src={profileimg} alt='profileimg' className='profile-img'/>
-                             <div className='customer-content'>
-                                 <p>Janaki Ramakrishna</p>
-                                 <p className='date'>Joined : 08/02/2024 </p>
-                             </div>
-                             
-                             <img src={dots} alt='dots' className='dotsimg'/>
-                        </div>
-                        <div className='customer-sub-details'>
-                             <img src={profileimg} alt='profileimg' className='profile-img'/>
-                             <div className='customer-content'>
-                                 <p>Janaki Ramakrishna</p>
-                                 <p className='date'>Joined : 08/02/2024 </p>
-                             </div>
-                             
-                             <img src={dots} alt='dots' className='dotsimg'/>
-                        </div>
-                        <div className='customer-sub-details'>
-                             <img src={profileimg} alt='profileimg' className='profile-img'/>
-                             <div className='customer-content'>
-                                 <p>Janaki Ramakrishna</p>
-                                 <p className='date'>Joined : 08/02/2024 </p>
-                             </div>
-                             
-                             <img src={dots} alt='dots' className='dotsimg'/>
-                        </div>
-                        <div className='customer-sub-details'>
-                             <img src={profileimg} alt='profileimg' className='profile-img'/>
-                             <div className='customer-content'>
-                                 <p>Janaki Ramakrishna</p>
-                                 <p className='date'>Joined : 08/02/2024 </p>
-                             </div>
-                             
-                             <img src={dots} alt='dots' className='dotsimg'/>
-                        </div>
-                   </div>
-                </div>
+  useEffect(() => {
+    // Fetch data from the API
+    fetch('https://jk-skills.onrender.com/users')
+      .then(response => response.json())
+      .then(data => setUsers(data))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []); 
+
+  const formatDOB = (dob) => {
+    const date = new Date(dob);
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    return date.toLocaleDateString('en-US', options);
+  };
+
+
+  return (
+    <>
+      <Adminheader />
+      <div className="admincustomer">
+        <h2>Customers</h2>
+        {/* <ul>
+         
+              <p>{JSON.stringify(users, null, 2)}</p>
+      
+        </ul> */}
+        <div className='main-con-users'>
+          {users.map(user => (
+            <div key={user.id} className='sub-con-users'>
+                <h1 className='username'>{user.fullName}</h1>
+                <p>Name : {user.fullName || 'N/A'}</p>
+                <p>Email : {user.email || 'N/A'}</p>
+                <p>Mobile Number :{user.mobileNumber}</p>
+                <p>Gender : {user.gender}</p>
+                <p>Date of Birth: {formatDOB(user.dateOfBirth)}</p>
+                <p>Location : {user.location}</p>
+    
             </div>
-        </>
-    )
-})
-export default Admincustomer
+          ))}
+        </div>
+        
+      </div>
+    </>
+  );
+};
+
+export default Admincustomer;
+
